8e3878e446200b0eb76db1af38682fbb385be3b8
Koi <koi271803@gmail.com>
2026-07-28 08:18:40 +0700

    Update Tile.h

--- src/Core/Tile.h
+++ src/Core/Tile.h
@@ -3,26 +3,26 @@
 
 #include "IMoveable.h"
 
-// Lớp Tile kế thừa từ Interface IMoveable (Tính Kế thừa)
+// Tính Kế thừa: Tile kế thừa IMoveable
 class Tile : public IMoveable {
-private:
-    // Tính Đóng gói (Encapsulation): Ẩn giá trị bên trong
+protected: 
     int value;
     int x;
     int y;
 
 public:
-    // Constructor
-    Tile(int startValue, int startX, int startY);
+    Tile(int val, int startX, int startY);
+    virtual ~Tile();
 
-    // Cung cấp các phương thức truy cập an toàn (Getter/Setter)
-    int getValue() const;
-    void setValue(int newValue);
-    
+    // Tính Đóng gói: Getter và Setter
+    virtual int getValue();
+    virtual void setValue(int newValue);
+
+    // THÊM MỚI: Khai báo 2 hàm getX và getY để khớp với file .cpp
     int getX() const;
     int getY() const;
 
-    // Ghi đè phương thức từ interface (Tính Đa hình có thể mở rộng từ đây)
+    // Tính Đa hình: Ghi đè hàm move từ interface
     void move(int dx, int dy) override;
 };
 
