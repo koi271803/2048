015d243304a14a78c3f69ed9a3126ae180b9a321
Koi <koi271803@gmail.com>
2026-07-29 12:57:56 +0700

    Update Tile.h

--- src/Core/Tile.h
+++ src/Core/Tile.h
@@ -3,27 +3,47 @@
 
 #include "IMoveable.h"
 
-// Tính Kế thừa: Tile kế thừa IMoveable
 class Tile : public IMoveable {
-protected: 
+protected:
     int value;
-    int x;
-    int y;
+    int x; // Đại diện cho Row (Hàng)
+    int y; // Đại diện cho Col (Cột)
 
 public:
     Tile(int val, int startX, int startY);
     virtual ~Tile();
 
-    // Tính Đóng gói: Getter và Setter
-    virtual int getValue();
+    virtual int getValue() const;
     virtual void setValue(int newValue);
-
-    // THÊM MỚI: Khai báo 2 hàm getX và getY để khớp với file .cpp
     int getX() const;
     int getY() const;
 
-    // Tính Đa hình: Ghi đè hàm move từ interface
     void move(int dx, int dy) override;
+};
+
+// THÊM MỚI: Lớp ObstacleTile kế thừa từ Tile
+class ObstacleTile : public Tile {
+private:
+    int weight; // Trọng số từ 1 đến 5
+
+public:
+    // Gọi constructor lớp cha với value = -1 để đánh dấu đây là đá
+    ObstacleTile(int startX, int startY, int initialWeight) : Tile(-1, startX, startY) {
+        weight = initialWeight;
+    }
+
+    // Hàm nhận sát thương
+    void takeDamage() {
+        if (weight > 0) {
+            weight--;
+            if (weight == 0) {
+                // Đá vỡ thì biến thành ô trống bình thường
+                this->value = 0;
+            }
+        }
+    }
+
+    int getWeight() const { return weight; }
 };
 
 #endif // TILE_H
